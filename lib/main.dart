44c14b8038a6f55e3d18e2d142a295733add5e0c
DavidSeifert <david.seifert@gmx.net>
2025-09-07 08:01:58 +0200

    First Functional Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'database_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,17 +33,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<int> _counter = [0,0,0,0];
+  final List<int> _counter = [0, 0, 0, 0];
+  final Databasehelper dbHelper = Databasehelper.instance;
 
   void _updateCounter(int index, int update) {
     setState(() {
       _counter[index] = update;
     });
-    
   }
-  void _writetoDatabase(){
 
+  void _writetoDatabase() {
+    dbHelper.addEntry(
+      _counter[0] * 1000 + _counter[1] * 100 + _counter[2] * 10 + _counter[3],
+    );
   }
+
+  Future<String> _readfromDatabase() async {
+    final calories = await dbHelper.getCaloriesForToday();
+    //dbHelper.showValues().then((value) => print(value));
+    return calories.toString();
+  }
+
+  Future<String> _showValues() async {
+    final values = await dbHelper.showValues();
+    dbHelper.showValues().then((value) => print(value));
+    return values.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,18 +69,36 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            FutureBuilder<String>(
+              future: _readfromDatabase(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Text(
+                    snapshot.data ?? 'No data',
+                    style: TextStyle(
+                      fontSize: 32, // bigger font size
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                }
+              },
+            ),
+            const SizedBox(height: 20), // space between total and button
+            ElevatedButton(onPressed: _writetoDatabase, child: Text("Add")),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: List.generate(4, (columnIndex) {
                 return SizedBox(
                   width: 50,
+                  height: 150,
                   child: CupertinoPicker(
                     itemExtent: 30,
-                    onSelectedItemChanged: (int newitem)
-                    {
+                    onSelectedItemChanged: (int newitem) {
                       _updateCounter(columnIndex, newitem);
                     },
                     children: List.generate(
@@ -72,11 +108,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 );
               }),
-            ), ElevatedButton(onPressed: _writetoDatabase, child: Text("Add"))
+            ),
           ],
         ),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showValues,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
